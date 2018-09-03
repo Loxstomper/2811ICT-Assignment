41e2fb9c6aadc5775c2a786617eb735e69ba71ff
loxstomper <lochieashcroft@gmail.com>
2018-09-04 03:03:50 +1000

    made login screen create user if not exists

--- front-end/src/app/admin/admin.component.ts
+++ front-end/src/app/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// import { HttpClient} from '@angular/common/http';
+import { HttpClient} from '@angular/common/http';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
@@ -28,48 +28,48 @@ export class AdminComponent implements OnInit {
     super_admins;
 
   // constructor(private router: Router, private http: HttpClient) { }
-  constructor(private router: Router) {}
+  constructor(private router: Router, private http:HttpClient) {}
 
 
-  // get_users() {
-  //   this.http.get('http://localhost:3000/api/users').subscribe(data => {
-  //     this.users = data['value'];
-  //   })
-  // }
+  get_users() {
+    this.http.get('http://localhost:3000/api/users').subscribe(data => {
+      this.users = data['value'];
+    })
+  }
 
-  // get_groups() {
-  //   this.http.get('http://localhost:3000/api/groups').subscribe(data => {
-  //     this.groups = data['value'];
-  //   })
-  // }
+  get_groups() {
+    this.http.get('http://localhost:3000/api/groups').subscribe(data => {
+      this.groups = data['value'];
+    })
+  }
 
-  // get_channels() {
-  //   this.http.get('http://localhost:3000/api/channels').subscribe(data => {
-  //     this.channels = data['value'];
-  //   })
-  // }
+  get_channels() {
+    this.http.get('http://localhost:3000/api/channels').subscribe(data => {
+      this.channels = data['value'];
+    })
+  }
 
-  // get_super_admins() {
-  //   this.http.get('http://localhost:3000/api/super_admins').subscribe(data => {
-  //     this.super_admins = data['value'];
-  //   })
-  // }
+  get_super_admins() {
+    this.http.get('http://localhost:3000/api/super_admins').subscribe(data => {
+      this.super_admins = data['value'];
+    })
+  }
 
-  // create_user(event)
-  // {
-  //   event.preventDefault();
+  create_user(event)
+  {
+    event.preventDefault();
 
-  //   this.http.post("http://localhost:3000/api/users/create", {username:this.username, super_admin:this.super_admin, email:this.email}).subscribe(
-  //     res=>{
-  //       if (res['success'] != "true")
-  //       {
-  //           alert(res['error']);
-  //       }
-  //     }
-  //   )
+    this.http.post("http://localhost:3000/api/users/create", {username:this.username, super_admin:this.super_admin, email:this.email}).subscribe(
+      res=>{
+        if (res['success'] != "true")
+        {
+            alert(res['error']);
+        }
+      }
+    )
 
-  //   console.log(this.username, this.email, this.super_admin);
-  // }
+    console.log(this.username, this.email, this.super_admin);
+  }
 
   ngOnInit() {
     this.viewer_username = localStorage.getItem("username");
--- front-end/src/app/home/home.component.ts
+++ front-end/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-home',
@@ -9,10 +10,21 @@ import { Router } from "@angular/router";
 export class HomeComponent implements OnInit {
 
   username:string = "";
+  email: string = "";
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private http:HttpClient) { }
 
   ngOnInit() {
+  }
+
+  create_user()
+  {
+    this.http.post("http://localhost:3000/api/users/create", {username:this.username, super_admin:0, email:this.email}).subscribe(
+      res=>{
+        // user does not need to know if its a new account
+      }
+    )
+
   }
 
   Login(event)
@@ -20,6 +32,7 @@ export class HomeComponent implements OnInit {
     event.preventDefault();
 
     localStorage.setItem('username', this.username);
+    this.create_user();
     this.router.navigateByUrl('chat');
   }
 
