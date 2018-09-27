17667e5e5a5684f9be2f036f257882632200a913
loxstomper <lochieashcroft@gmail.com>
2018-09-27 14:35:04 +1000

    have login working

--- index.js
+++ index.js
@@ -17,7 +17,27 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// socket stuff
+var login_component = require("./login.js")();
+var users_component = require("./users.js")();
+
+// connect to mongodb
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/ass2";
+
+MongoClient.connect(url, {poolSize:10}, function(err, client) {
+  if (err) throw err;
+
+  const dbName = "ass2";
+  const db = client.db(dbName);
+
+  login_component.set_db(db);
+  users_component.set_db(db);
+
+
+
+  // db.close();
+});
+
 io.on('connection', (socket) => {
     console.log('a user connected');
 
@@ -35,6 +55,15 @@ io.on('connection', (socket) => {
         io.emit('message', {type:'message', text:message});
     });
 });
+
+app.post("/api/login", function(req, res){
+  login_component.login(req.body.username, req.body.password, res);
+});
+
+app.post("/api/users/create", function(req, res){
+  users_component.create_user("bob", "bob@gmail.com", res);
+});
+
 
 
 http.listen(3000);
